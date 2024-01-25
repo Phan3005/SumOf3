531203aa080c2343d43864c28d4ec5f1a1abde48
Erik Phan <erikph003@ou.edu>
2024-01-25 11:20:45 -0600

    Add project files.

---
+++ SumOf3/Program.cs
@@ -0,0 +1,25 @@
+﻿
+Console.WriteLine("Write your first number!");
+string firstNumberAsString = Console.ReadLine();
+
+double firstNumber = Convert.ToDouble(firstNumberAsString);
+
+Console.WriteLine("Write your second number!");
+string secondNumberAsString = Console.ReadLine();
+
+double secondNumber = Convert.ToDouble(secondNumberAsString);
+
+Console.WriteLine("Write your third number!");
+string thirdNumberAsString = Console.ReadLine();
+
+double thirdNumber = Convert.ToDouble(thirdNumberAsString);
+
+double sum = firstNumber + secondNumber + thirdNumber;
+
+Console.WriteLine($"The total of your numbers is {sum.ToString("N3")}!");
+
+const double tripleSeven = 7.777;
+
+double final = sum * tripleSeven;
+Console.WriteLine($"{sum.ToString("N3")} times {tripleSeven.ToString("N3")} is {final.ToString("N3")}!");
+
